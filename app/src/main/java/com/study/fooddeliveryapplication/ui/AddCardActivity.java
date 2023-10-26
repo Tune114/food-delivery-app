61c5d3b08e7878efe68a69d5051e0d6d5d51cb41
Tu ne! <a0934805315@gmail.com>
2023-10-26 20:13:54 +0700

    fix text size

--- app/src/main/java/com/study/fooddeliveryapplication/ui/AddCardActivity.java
+++ app/src/main/java/com/study/fooddeliveryapplication/ui/AddCardActivity.java
@@ -17,7 +17,7 @@ public class AddCardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addcard);
+        setContentView(R.layout.activity_add_card);
 
         btnBack= (ImageView)findViewById(R.id.btn_back);
 
