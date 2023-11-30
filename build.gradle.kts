2b6768098f6ff1646aaafe9b80cd8e6191574246
CongMinhhh <126494802+CongMinhhh@users.noreply.github.com>
2023-11-30 17:17:17 +0700

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
+<<<<<<< HEAD
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+=======
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.2" apply false
+>>>>>>> ac813fb6b8976458df553e8f68ddd5e59f26d5ca
 }
