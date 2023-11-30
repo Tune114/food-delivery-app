c82ed8ffee474faee523abd2341c4be16018ef4a
CongMinhhh <126494802+CongMinhhh@users.noreply.github.com>
2023-11-30 17:18:27 +0700

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("com.android.application")
+<<<<<<< HEAD
+    id("com.google.gms.google-services")
+=======
+>>>>>>> ac813fb6b8976458df553e8f68ddd5e59f26d5ca
 }
 
 android {
@@ -26,12 +30,34 @@ android {
         }
     }
     compileOptions {
+<<<<<<< HEAD
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+=======
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+>>>>>>> ac813fb6b8976458df553e8f68ddd5e59f26d5ca
     }
 }
 
 dependencies {
+<<<<<<< HEAD
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("com.firebaseui:firebase-ui-database:8.0.2")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+=======
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -41,4 +67,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("androidx.cardview:cardview:1.0.0")
+>>>>>>> ac813fb6b8976458df553e8f68ddd5e59f26d5ca
 }
