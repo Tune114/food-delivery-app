01361074f9870819509b2b181965a22575c7318e
CongMinhhh <126494802+CongMinhhh@users.noreply.github.com>
2023-11-30 17:37:05 +0700

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,6 @@
 plugins {
     id("com.android.application")
-<<<<<<< HEAD
     id("com.google.gms.google-services")
-=======
->>>>>>> ac813fb6b8976458df553e8f68ddd5e59f26d5ca
 }
 
 android {
@@ -30,18 +27,12 @@ android {
         }
     }
     compileOptions {
-<<<<<<< HEAD
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-=======
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
->>>>>>> ac813fb6b8976458df553e8f68ddd5e59f26d5ca
     }
 }
 
 dependencies {
-<<<<<<< HEAD
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -57,15 +48,4 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.squareup.picasso:picasso:2.71828")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
-=======
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation("androidx.cardview:cardview:1.0.0")
->>>>>>> ac813fb6b8976458df553e8f68ddd5e59f26d5ca
 }
