cedf2d928235e6e6ca7fc945a6b6ffe2531b94ff
CongMinhhh <126494802+CongMinhhh@users.noreply.github.com>
2023-12-02 09:59:04 +0700

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -41,12 +41,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.firebaseui:firebase-ui-database:8.0.2")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.cardview:cardview:1.0.0")
+
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.squareup.picasso:picasso:2.71828")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
-    implementation("com.google.firebase:firebase-analytics")
 }
+
