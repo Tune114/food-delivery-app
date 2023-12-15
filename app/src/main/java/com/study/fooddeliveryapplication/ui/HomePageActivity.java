ee58fc1006a2638b5dd069fad984e85d542ab37b
DoVietThang2k3 <145999416+DoVietThang2k3@users.noreply.github.com>
2023-12-15 23:33:34 +0700

    Update HomePageActivity.java
    update new

--- app/src/main/java/com/study/fooddeliveryapplication/ui/HomePageActivity.java
+++ app/src/main/java/com/study/fooddeliveryapplication/ui/HomePageActivity.java
@@ -1,34 +1,48 @@
 package com.study.fooddeliveryapplication.ui;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.FirebaseDatabase;
 import com.study.fooddeliveryapplication.R;
 import com.study.fooddeliveryapplication.adapter.RestauItemAdapter;
 import com.study.fooddeliveryapplication.adapter.CustomSpinnerAdapter;
 import com.study.fooddeliveryapplication.adapter.itemAdapter;
-import com.study.fooddeliveryapplication.model.Item;
 import com.study.fooddeliveryapplication.model.RestauItem;
 
 public class HomePageActivity extends AppCompatActivity {
     TextView et_search;
-    ImageView cart;
+    ImageView cart, iv_menu;
     itemAdapter adapter1;
+    DrawerLayout drawerLayout;
+    LinearLayout lnHome, lnCart, lnRestaurant, lnProfile;
+    ConstraintLayout constraintLayout;
+    TextView tv_more_res;
     RestauItemAdapter adapter2;
     @SuppressLint("MissingInflatedId")
     @Override
@@ -42,51 +56,28 @@ public class HomePageActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        tv_more_res = (TextView) findViewById(R.id.tv_see_all_res);
+        tv_more_res.setOnClickListener(view -> {
+            Intent intent = new Intent(HomePageActivity.this, RestaurantList.class);
+            startActivity(intent);
+        });
+
         cart = (ImageView)findViewById(R.id.iv_bag);
         cart.setOnClickListener(view -> {
             Intent intent = new Intent(HomePageActivity.this, AddCartActivity.class);
             startActivity(intent);
         });
 
-
-        //spinner
-        String[] options = {"Option 1", "Option 2", "Option 3"};
-        CustomSpinnerAdapter adapter = new CustomSpinnerAdapter(this, android.R.layout.simple_spinner_item, options);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner spinner = findViewById(R.id.sp_place_deliver);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedOption = (String) parent.getItemAtPosition(position);
-                Toast.makeText(getApplicationContext(), "You Picked: " + selectedOption, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Xử lý khi không có tùy chọn nào được chọn
-            }
-        });
-
+        SharedPreferences sharedPreferences = getSharedPreferences("my_app_preferences", Context.MODE_PRIVATE);
+        String Name = sharedPreferences.getString("name", null);
         //Custom hello text
         TextView tvhello = findViewById(R.id.tv_hello_text);
-        String fullText = "Hey Quang, Good Morning!";
+        String fullText = "Hello, "+ Name;
         SpannableString spannableString = new SpannableString(fullText);
         int startIndex = fullText.indexOf(",");
         int endIndex = fullText.length();
         spannableString.setSpan(new StyleSpan(Typeface.BOLD), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         tvhello.setText(spannableString);
-
-        //Custom Items
-        RecyclerView recyclerView1 = findViewById(R.id.recyclerView1);
-        LinearLayoutManager layoutManager1 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerView1.setLayoutManager(layoutManager1);
-        FirebaseRecyclerOptions<Item> options1 =
-                new FirebaseRecyclerOptions.Builder<Item>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("/HomePage/HomePage_FoodItems"),Item.class)
-                        .build();
-        adapter1 = new itemAdapter(options1);
-        recyclerView1.setAdapter(adapter1);
 
 
         //Custom Restau Items
@@ -95,23 +86,85 @@ public class HomePageActivity extends AppCompatActivity {
         recyclerView2.setLayoutManager(layoutManager2);
         FirebaseRecyclerOptions<RestauItem> options2 =
                 new FirebaseRecyclerOptions.Builder<RestauItem>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("/HomePage/HomePage_RestItems"),RestauItem.class)
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("/restaurants"),RestauItem.class)
                         .build();
         adapter2 = new RestauItemAdapter(options2);
         recyclerView2.setAdapter(adapter2);
+        // bottomNavigationView
+        drawerLayout = findViewById(R.id.drawLayout);
+        iv_menu = findViewById(R.id.iv_menu);
+        lnHome = findViewById(R.id.home);
+        lnCart = findViewById(R.id.cart);
+        lnProfile = findViewById(R.id.profile);
+        lnRestaurant = findViewById(R.id.restaurant);
+        constraintLayout = findViewById(R.id.dontknow);
+        constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("MyApp", "is oke");
+            }
+        });
+        iv_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDrawer(drawerLayout);
+            }
+        });
+        lnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recreate();
+            }
+        });
+        lnCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActitvity(HomePageActivity.this, AddCartActivity.class);
+            }
+        });
+        lnRestaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActitvity(HomePageActivity.this, RestaurantList.class);
+            }
+        });
+        lnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActitvity(HomePageActivity.this, UserProflie.class);
+            }
+        });
+    }
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public static void redirectActitvity(Activity activity, Class secondActivity){
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        adapter1.startListening();
         adapter2.startListening();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        adapter1.stopListening();
         adapter2.stopListening();
     }
 }
